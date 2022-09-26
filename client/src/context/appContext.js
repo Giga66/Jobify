6d166ad01d1543b172550a507ce3ee540dee2c32
Giga <gigamail324@gmail.com>
2022-09-27 01:54:10 +0300

    login user functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -3,7 +3,7 @@ import { useReducer, useContext } from "react"
 import axios from 'axios'
 
 import reducer from "./reducer"
-import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, RESGISTER_USER_SUCCESS, REGISTER_USER_ERROR, REGISTER_USER_SUCCESS } from "./actions"
+import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_ERROR, REGISTER_USER_SUCCESS, LOGIN_USER_BEGIN, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR  } from "./actions"
 
 
 const token = localStorage.getItem('token')
@@ -62,9 +62,13 @@ const AppProvider = ({ children }) => {
         clearAlert()
     }
 
+    const loginUser = async (currentUser) =>{
+        console.log(currentUser);
+    }
+
 
     return (
-        <AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
+        <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser }}>
             {children}
         </AppContext.Provider>
     )

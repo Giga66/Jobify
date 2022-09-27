1e802d793fa9ec25af0468a9e10b57df55902453
Giga <gigamail324@gmail.com>
2022-09-27 13:32:07 +0300

    nested pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import {Error, Register, Dashboard, Landing} from './pages'
+import { Error, Register, Landing } from './pages'
+import { AddJob, AllJobs, Profile, SharedLayout, Stats } from './pages/dashboard'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='stats' element={<Stats />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+        </Route>
+
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />

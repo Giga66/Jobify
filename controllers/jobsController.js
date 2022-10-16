f494a6fbda6c2860a683fcc5b0160c28ed0f4689
Giga <gigamail324@gmail.com>
2022-10-17 02:00:16 +0300

    work on delete-job front end

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -17,8 +17,20 @@ const createJob = async (req, res) => {
 
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const { id: jobId } = req.params
+
+    const job = await Job.findOne({ _id: jobId })
+
+    if (!job) {
+        throw new CustomError.NotFoundError(`No job with id : ${jobId}`)
+    }
+
+    checkPermissions(req.user, job.createdBy)
+
+    await job.remove()
+    res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' })
 }
+
 const getAllJobs = async (req, res) => {
     const jobs = await Job.find({ createdBy: req.user.userId })
 
@@ -26,20 +38,25 @@ const getAllJobs = async (req, res) => {
 }
 const updateJob = async (req, res) => {
     const { id: jobId } = req.params
+    const { company, position, status } = req.body
 
-    const { company, position } = req.body
-
-    if (!company || !position) {
-        throw new BadRequestError('Please Provide All Values')
+    if (!position || !company) {
+        throw new BadRequestError('Please provide all values')
     }
-
     const job = await Job.findOne({ _id: jobId })
 
     if (!job) {
-        throw new NotFoundError(`No job with id ${jobId}`)
+        throw new NotFoundError(`No job with id :${jobId}`)
     }
 
     // check permissions
+    // req.user.userId (string) === job.createdBy(object)
+    // throw new UnAuthorizedError('Not authorized to access this route')
+
+    // console.log(typeof req.user.userId)
+    // console.log(typeof job.createdBy)
+
+    checkPermissions(req.user, job.createdBy)
 
     const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
         new: true,
@@ -48,6 +65,7 @@ const updateJob = async (req, res) => {
 
     res.status(StatusCodes.OK).json({ updatedJob })
 }
+
 const showStats = async (req, res) => {
     res.send('show stats')
 }

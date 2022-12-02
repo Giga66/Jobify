a472d9c2464591122ced161841ab055852c0a4c1
giga <gigamail324@gmail.com>
2022-12-02 20:36:15 +0200

    heroku upload fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './db/connect.js'
 import 'express-async-errors'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -61,7 +62,12 @@ const port = process.env.PORT || 5000
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URI, {useNewURLParser: true})
+        mongoose.connection.once('open', function(){
+            console.log('Conection has been made!');
+             }).on('error', function(error){
+          console.log('Error is: ', error)
+        })
         app.listen(port, () => {
             console.log(`Server is listening on ${port}`)
         })
